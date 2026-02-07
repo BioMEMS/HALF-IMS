f5fcc269231881f6265f1ec2e5436bd7e1e6c2a5
Joseph Haun <josephhaun565@gmail.com>
2026-02-07 05:36:59 -0800

    Updating to better allow multiple settings files.

--- simulations/SIMION/auto-run-server.py
+++ simulations/SIMION/auto-run-server.py
@@ -7,14 +7,17 @@ import shutil
 import subprocess
 import re
 import datetime
+import glob
 
 if (len(sys.argv) < 2 or not os.path.isdir(sys.argv[1])):
     print("Please provide a valid target directory for SIMION configuration files.")
     sys.exit(1)
 
 # Build the target file path
-target_file="settings.config"
-target_path=os.path.abspath(sys.argv[1] + "/" + target_file)
+target_file_extension=".config"
+completed_file_extension=".completed"
+target_file="settings" + target_file_extension
+target_path=os.path.abspath(sys.argv[1] + "/*" + target_file_extension)
 simulation_directory_path=os.path.abspath(os.path.dirname(__file__) + "/base/")
 destination_path=os.path.abspath(simulation_directory_path + "/" + target_file)
 
@@ -35,12 +38,14 @@ os.chdir(simulation_directory_path);
 while True:
     time.sleep(30)
     print("Timestamp:",datetime.datetime.now())
+    target_file_list = glob.glob(target_path)
+    
     # If the configuration file exists in the target path
-    if(os.path.isfile(target_path)):
+    if(len(target_file_list) > 0):
         time.sleep(10)
-        # Move the newly detected configuration file
+        # Copy the newly detected configuration file
         print("New configuration file found.")
-        shutil.move(target_path, destination_path)
+        shutil.copyfile(target_file_list[0], destination_path)
 
         # Execute the SIMION command with the new configuration file
         print("Running new configuration file.")
@@ -53,6 +58,9 @@ while True:
                 print("Moving", item)
                 shutil.move(os.path.abspath(simulation_directory_path + "/" + item), os.path.abspath(sys.argv[1]))
                 
-                
+        # Once finished, change the file extension to prevent re-runs while preserving a log of what was run
+        print("Removing configuration file from runtime list.")
+        shutil.move(target_file_list[0], target_file_list[0] + completed_file_extension)
+
                 
 
